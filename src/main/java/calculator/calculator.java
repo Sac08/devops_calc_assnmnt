50c110f0ed3da630782e7a44e75990f587bf95f1
sac <sachinbagalakoti08@gmail.com>
2020-04-04 11:51:22 +0530

    class name resolved

--- src/main/java/calculator/calculator.java
+++ src/main/java/calculator/calculator.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 import java.lang.Exception;
 
-public class Main {
+public class calculator {
 	public static void main(String args[])
 	{
 		System.out.println("Welcome to Calculator App!");
