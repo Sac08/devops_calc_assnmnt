c7a7a6474b4804e72e2a62652b72b1f77cfb28e7
Sachin Bagalakoti <Sachinbagalakoti08+codecamp@gmail.com>
2020-05-09 11:07:59 +0530

    adding divide operation

--- src/main/java/calculator/calculator.java
+++ src/main/java/calculator/calculator.java
@@ -2,7 +2,18 @@ package calculator;
 import java.util.Scanner;
 
 public class calculator {
-
+    public long add(long first, long second) {
+        return first + second;
+    }
+    public long subtract(long first, long second) {
+        return first - second;
+    }
+    public long multiply(long first, long second) {
+        return first * second;
+    }
+    public long divide(long first, long second) {
+        return first / second;
+    }
     public static void main(String[] args) {
 
         Scanner reader = new Scanner(System.in);
@@ -31,6 +42,61 @@ public class calculator {
                 result = first * second;
                 break;
 
+
+            // operator doesn't match any case constant (+, -, *, /)
+            default:
+                System.out.printf("Error! operator is not correct");
+                return;
+        }
+
+        System.out.printf("%d %c %d = %d", first, operator, second, result);
+    }
+}
+import java.util.Scanner;
+
+public class calculator {
+    public static int add(int first, int second) {
+        return first + second;
+    }
+    public static int subtract(int first, int second) {
+        return first - second;
+    }
+    public static int multiply(int first, int second) {
+        return first * second;
+    }
+    public static int divide(int first, int second) {
+        return first / second;
+    }
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter two numbers: ");
+
+        // nextDouble() reads the next double from the keyboard
+        int first = reader.nextInt();
+        int second = reader.nextInt();
+
+        System.out.print("Enter an operator (+, -, *, /): ");
+        char operator = reader.next().charAt(0);
+
+        int result;
+
+        switch(operator)
+        {
+            case '+':
+                result = add(first, second);
+                break;
+
+            case '-':
+                result = subtract(first, second);
+                break;
+
+            case '*':
+                result = multiply(first, second);
+                break;
+            case '/':
+            	result = divide(first, second);
+            	break;
 
             // operator doesn't match any case constant (+, -, *, /)
             default:
